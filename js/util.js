d2167bf65053d2bef7604b87a187090505667f6e
yuyang2999 <yuyang2999@users.noreply.github.com>
2018-03-05 11:20:02 +0800

    修改

--- js/util.js
+++ js/util.js
@@ -3,9 +3,10 @@
 	* 定义全局常量
 */
 
-var urlPort = "http://localhost:8080/";/*接口*/
+var urlPort = "http://localhost:8080/"; /*接口*/
 
-var urlContent ="RIIS-FM/fmTradeOrder/selectTradeOrder";/*通过iSenderId查询全部订单信息，并分页显示*/
+var urlOrderInfo ="RIIS-FM/fmTradeOrder/selectTradeOrder"; /*通过iSenderId查询全部订单信息，并分页显示*/
+
 
 /**
 	* 定义函数
@@ -29,10 +30,11 @@ function postData(url, data, callback) {
 	* 获取url
 	* @return port
 */
-function getPort(){
-	var url = urlPort+urlContent;
+function getOrderInfoPort(){
+	var url = urlPort+urlOrderInfo;
 	return url;
 }
+
 
 /**
 	* 订单类型转换
@@ -43,28 +45,28 @@ function getPort(){
 function orderType(value){
 	var ret;
 	switch (value){
-		case '3':
+		case 3:
 			ret = "集装箱运输";
 			break;
-		case '4':
+		case 4:
 			ret = "高铁快运";
 			break;
-		case '5':
+		case 5:
 			ret = "特快货物班列";
 			break;
-		case '6':
+		case 6:
 			ret = "货物快运";
 			break;
-		case '7':
+		case 7:
 			ret = "散装堆货物";
 			break;
-		case '8':
+		case 8:
 			ret = "批量成件货物";
 			break;
-		case '9':
+		case 9:
 			ret = "液体货物";
 			break;
-		case '10':
+		case 10:
 			ret = "整车大宗货物直达";
 			break;
 		default:
@@ -83,31 +85,31 @@ function orderType(value){
 function orderState(value){
 	var ret;
 	switch (value){
-		case '0':
+		case 0:
 			ret = "草稿";
 			break;
-		case '1':
+		case 1:
 			ret = "审核中";
 			break;
-		case '2':
+		case 2:
 			ret = "确认承运方案";
 			break;
-		case '3':
+		case 3:
 			ret = "已审核未通过";
 			break;
-		case '4':
+		case 4:
 			ret = "待付款";
 			break;
-		case '5':
+		case 5:
 			ret = "运输中";
 			break;
-		case '6':
+		case 6:
 			ret = "待评价";
 			break;
-		case '7':
+		case 7:
 			ret = "已取消";
 			break;
-		case '7':
+		case 8:
 			ret = "已完成";
 			break;
 		default:
